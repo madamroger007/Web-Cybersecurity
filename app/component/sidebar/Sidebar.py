f23a89acf9a89eaae38f271670a428274886d97c
madam007 <adamsetiadijr07@gmail.com>
2023-07-28 00:37:51 +0700

    update caption sidebar

--- app/component/sidebar/Sidebar.py
+++ app/component/sidebar/Sidebar.py
@@ -1,3 +1,3 @@
 def Sidebar(st, time): 
     with st.sidebar:
-        st.caption("""Dev by :blue[_Adam_:hatching_chick:] \n Original project from :blue[_Lorenza_ :monkey_face:]""")
+        st.caption("""Dev by :blue[_Adam_:hatching_chick:] \n Original project from :blue[_mymajikan_ :monkey_face:]""")
